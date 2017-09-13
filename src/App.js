f8cf6d79e459e82b5acad0bb3eb5359618441b8f
Aaron Mc Adam <aaron@aaronmcadam.com>
2017-09-13 21:07:09 +0100

    feat: add global styles
    * feat: add colours
    * chore(dependencies): add styled-components and polished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
 import React, { Component } from 'react';
+import styled, { injectGlobal } from 'styled-components';
+import { normalize } from 'polished';
+
+const honeyFlower = '#4A1479';
+const burningSand = '#D0806A';
+const vistaBlue = '#79E7A9';
+const bleuDeFrance = '#2386EC';
+
+injectGlobal`
+  ${normalize()}
+
+  body {
+    background-color: ${honeyFlower};
+    color: ${burningSand};
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-size: 16px;
+    margin: 0;
+  }
+`;
 
 class App extends Component {
   render() {
