80a32536512bcf13f1137bdacd944d1a82d287fd
Aaron Mc Adam <aaron@aaronmcadam.com>
2017-09-13 21:02:33 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>SearchFilm</h2>
+        </div>
+        <p>Search for a film!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
